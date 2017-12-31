da8dcecd85aebee9f3fc5c77029fb809a6cb5d0e
Margarida <margarida.ns@hotmail.com>
2017-12-31 00:52:12 +0000

    commit registo e login

---
+++ app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/MainActivityUser.java
@@ -0,0 +1,21 @@
+package pt.ipp.estg.projeto_kidszone.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import pt.ipp.estg.projeto_kidszone.R;
+
+/**
+ * Created by margarida on 30/12/2017.
+ */
+
+public class MainActivityUser extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_user);
+
+    }
+}
