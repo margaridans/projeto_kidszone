8c4afd12ddcd1957a2160eb24823062f0966f895
Margarida <margarida.ns@hotmail.com>
2018-01-02 22:30:47 +0000

    atualizacao login com facebook

--- app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/Login_Registo/Login.java
+++ app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/Login_Registo/Login.java
@@ -2,8 +2,13 @@ package pt.ipp.estg.projeto_kidszone.Activities.Login_Registo;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +20,9 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import projeto_kidszone.database_library.Database.MyDbHelper;
 import pt.ipp.estg.projeto_kidszone.MainActivity;
@@ -69,6 +77,22 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 }
             }
         });
+
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "pt.ipp.estg.projeto_kidszone",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
 
         //Login com Facebook
