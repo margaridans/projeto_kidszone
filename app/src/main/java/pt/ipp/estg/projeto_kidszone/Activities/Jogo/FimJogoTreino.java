430cc81d38c97013ce1e4ed1a9c57eb1e5b57616
Marisa <marisa_machado95@hotmail.com>
2018-01-04 13:55:40 +0000

    teste

--- app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/Jogo/FimJogoTreino.java
+++ app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/Jogo/FimJogoTreino.java
@@ -3,6 +3,8 @@ package pt.ipp.estg.projeto_kidszone.Activities.Jogo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,7 +12,9 @@ import android.widget.Toast;
 import pt.ipp.estg.projeto_kidszone.R;
 
 public class FimJogoTreino extends AppCompatActivity {
-    TextView txtPontuacao;
+    TextView txtPontuacao, txtCompetitivo, txtNovoTreino;
+    Button btnNovoTreino;
+    Button btnComp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +26,35 @@ public class FimJogoTreino extends AppCompatActivity {
 
         txtPontuacao = (TextView) findViewById(R.id.txtPontuacao);
 
-        //<20 ainda n esta preparado para competir -> que tal umn novo jogo treino
+        btnNovoTreino = (Button) findViewById(R.id.btnNovoTreino);
+        btnComp = (Button) findViewById(R.id.btnCompetitivo);
+
         //>20
 
         txtPontuacao.setText("A sua pontuacao é de: " + pontuacao);
 
+        if (pontuacao <= 20) {
+            clickBtnMostar1();
+        }
+    }
 
+
+    public void clickBtnMostar1(View view) {
+        txtCompetitivo.setVisibility(View.VISIBLE);
+        txtNovoTreino.setText("Ainda não está preparado para competir. Que tal um novo jogo treino?");
+
+        btnNovoTreino.setVisibility(View.VISIBLE);
+    }
+
+    public void clickBtnNaoMostar1(View esc) {
+        btnNovoTreino.setVisibility(View.INVISIBLE);
+    }
+
+    public void clickBtnMostar2(View view) {
+        btnNovoTreino.setVisibility(View.VISIBLE);
+    }
+
+    public void clickBtnNaoMostar2(View esc) {
+        btnNovoTreino.setVisibility(View.INVISIBLE);
     }
 }
