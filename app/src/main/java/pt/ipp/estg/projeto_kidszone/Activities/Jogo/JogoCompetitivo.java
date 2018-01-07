e5b31a34e3812980635b079dfc8fd13f60217d86
Marisa <marisa_machado95@hotmail.com>
2018-01-07 18:27:44 +0000

    jogo competitivo

--- app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/Jogo/JogoCompetitivo.java
+++ app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/Jogo/JogoCompetitivo.java
@@ -1,14 +1,17 @@
 package pt.ipp.estg.projeto_kidszone.Activities.Jogo;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import pt.ipp.estg.projeto_kidszone.Fragments.JogoCompDificFragment;
+import pt.ipp.estg.projeto_kidszone.Fragments.JogoCompetitivoFragment;
 import pt.ipp.estg.projeto_kidszone.R;
 
-public class JogoCompetitivo {
-
-
+public class JogoCompetitivo extends FragmentActivity  {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_competitivo);
+    }
 }
