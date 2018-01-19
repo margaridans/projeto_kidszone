fc5f22bb2393f1ac4491a0a542253e7887110535
Marisa <marisa_machado95@hotmail.com>
2018-01-19 02:19:56 +0000

    notificacao

--- app/src/main/java/pt/ipp/estg/projeto_kidszone/MainActivity.java
+++ app/src/main/java/pt/ipp/estg/projeto_kidszone/MainActivity.java
@@ -5,20 +5,20 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import java.net.URI;
 
 import projeto_kidszone.database_library.Database.MyDbHelper;
 import pt.ipp.estg.projeto_kidszone.Activities.Dicas.DicasActivity;
 import pt.ipp.estg.projeto_kidszone.Activities.Jogo.MenuJogo;
-import pt.ipp.estg.projeto_kidszone.Activities.Login_Registo.ActivityMainUser;
 import pt.ipp.estg.projeto_kidszone.Activities.Login_Registo.Login;
-
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -33,25 +33,51 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         myDb = new MyDbHelper(this);
 
-        Button btnLogin =  findViewById(R.id.btnLogin);
+        Button btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
 
-        Button btnJogar =  findViewById(R.id.btnJogar);
+        Button btnJogar = findViewById(R.id.btnJogar);
         btnJogar.setOnClickListener(this);
 
-        Button btnDicas =  findViewById(R.id.btnDicas);
+        Button btnDicas = findViewById(R.id.btnDicas);
         btnDicas.setOnClickListener(this);
 
-
-       /* SharedPreferences prefs = getSharedPreferences("login", MODE_PRIVATE);
-        String nome = prefs.getString("username", "default");
-
-        if(nome!=null) {
-            Intent itLogin = new Intent(this, ActivityMainUser.class);
-            startActivity(itLogin);
-        }*/
+    }
 
 
+    public void gerarNotificao(View view) {
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        Intent vamosJogar = new Intent(this, MenuJogo.class);
+        vamosJogar.setAction( "action-ok-vamos" );
+        PendingIntent vamosJogarPI = PendingIntent.getActivity(this,0,vamosJogar,0);
+        NotificationCompat.Action vamosJogarAC = new NotificationCompat.Action(R.drawable.entrar_noti,"Sim, vamos",vamosJogarPI);
+
+        Intent cancelar = new Intent(this, MainActivity.class);
+        vamosJogar.setAction( "action-cancelar" );
+        PendingIntent cancelarPI = PendingIntent.getActivity(this,0,cancelar,0);
+        NotificationCompat.Action cancelarAC = new NotificationCompat.Action(R.drawable.cancel_noti,"Cancelar",cancelarPI);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+
+        builder.setContentTitle("Vamos jogar?");
+        builder.setContentText("Que tal pores o teu conhecimento à prova?");
+        builder.setSmallIcon(R.drawable.logo);
+        builder.setContentIntent(vamosJogarPI);
+        builder .addAction(vamosJogarAC);
+        builder .addAction(cancelarAC);
+        Notification n = builder.build();
+        n.vibrate = new long[]{150, 300, 150, 600};
+        nm.notify(R.drawable.sad, n);
+
+        try {
+            Uri som = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone toque = RingtoneManager.getRingtone(this, som);
+            toque.play();
+        } catch (Exception e) {
+        }
     }
 
     @Override
