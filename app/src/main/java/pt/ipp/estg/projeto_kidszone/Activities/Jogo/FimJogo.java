913c0350e0a05fcafaf114a12507d868d2fcb31a
Marisa <marisa_machado95@hotmail.com>
2018-01-21 18:56:09 +0000

    akfi

--- app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/Jogo/FimJogo.java
+++ app/src/main/java/pt/ipp/estg/projeto_kidszone/Activities/Jogo/FimJogo.java
@@ -44,9 +44,6 @@ public class FimJogo extends AppCompatActivity implements View.OnClickListener {
         Integer ultima_pont = prefs_ultimapont.getInt("pontuacao", 0);
 
 
-        txtPontuacao.setText("A sua pontuacao é de: " + pontuacao_nova);
-
-
         myDb = new MyDbHelper(this);
         if (nome != null) {
             Boolean res = myDb.inserirPontuacao(pontuacao_nova, nome);
@@ -59,13 +56,13 @@ public class FimJogo extends AppCompatActivity implements View.OnClickListener {
                     AlertDialog.Builder alertaPont = new AlertDialog.Builder(FimJogo.this);
                     alertaPont.setTitle("Boaaaaa! ");
                     alertaPont.setIcon(R.drawable.happy);
-                    alertaPont.setMessage("Conseguiste superar a tua última pontuação!");
+                    alertaPont.setMessage("A tua pontuação foi de " + pontuacao_nova + ". Conseguiste superar a tua última pontuação.");
                     alertaPont.setCancelable(false);
                     alertaPont.setNegativeButton("Fechar", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            onResume();
-
+                            Intent intentMenu = new Intent(FimJogo.this, MenuJogo.class);
+                            startActivity(intentMenu);
                         }
                     });
                     alertaPont.setPositiveButton("Voltar a jogar", new DialogInterface.OnClickListener() {
@@ -84,17 +81,17 @@ public class FimJogo extends AppCompatActivity implements View.OnClickListener {
                     AlertDialog.Builder alertaPont = new AlertDialog.Builder(FimJogo.this);
                     alertaPont.setTitle("Oh nãooooo! ");
                     alertaPont.setIcon(R.drawable.sad);
-                    alertaPont.setMessage("não conseguiste superar a tua última pontuação!");
+                    alertaPont.setMessage("A tua pontuação foi de " + pontuacao_nova + ". Não conseguiste superar a tua última pontuação!");
                     alertaPont.setCancelable(false);
 
                     alertaPont.setNegativeButton("Fechar", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-
-                            onResume();
-
+                            Intent intentMenu = new Intent(FimJogo.this, MenuJogo.class);
+                            startActivity(intentMenu);
                         }
                     });
+
 
                     alertaPont.setPositiveButton("Voltar a jogar", new DialogInterface.OnClickListener() {
                         @Override
@@ -112,12 +109,13 @@ public class FimJogo extends AppCompatActivity implements View.OnClickListener {
                     AlertDialog.Builder alertaPont = new AlertDialog.Builder(FimJogo.this);
                     alertaPont.setTitle("Que pontaria! ");
                     alertaPont.setIcon(R.drawable.sad);
-                    alertaPont.setMessage("conseguiste fazer a mesma pontuação da última vez!");
+                    alertaPont.setMessage("A tua pontuação foi de " + pontuacao_nova + ". Conseguiste fazer a mesma pontuação da última vez!");
                     alertaPont.setCancelable(false);
                     alertaPont.setNegativeButton("Fechar", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            onResume();
+                            Intent intentMenu = new Intent(FimJogo.this, MenuJogo.class);
+                            startActivity(intentMenu);
                         }
                     });
                     AlertDialog alertDialogo = alertaPont.create();
